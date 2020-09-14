41da390505ce0d175b791918285c1c295dd88f89
zhaorenjie <zhaorenjie@MacBook-Pro-2.local>
2020-09-14 15:18:38 +0800

    inital

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const { generateAcceptValue, parseMessage, send, close } = require("./util");
+const port = 8886;
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    if (err) {
+      console.log(err);
+      res.end("500");
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.on("upgrade", function (req, socket) {
+  if (req.headers["upgrade"] !== "websocket") {
+    socket.end("HTTP/1.1 400 Bad Request");
+    return;
+  }
+
+  socket.on("data", (buffer) => {
+    parseMessage(buffer);
+  });
+
+  setTimeout(() => {
+    socket.write(send("我是小白"));
+  }, 3000);
+  setTimeout(() => {
+    socket.write(close());
+  }, 5000);
+  // 读取客户端提供的Sec-WebSocket-Key
+  const secWsKey = req.headers["sec-websocket-key"];
+  // 使用SHA-1算法生成Sec-WebSocket-Accept
+  const hash = generateAcceptValue(secWsKey);
+  // 设置HTTP响应头
+  const responseHeaders = [
+    "HTTP/1.1 101 Web Socket Protocol Handshake",
+    "Upgrade: WebSocket",
+    "Connection: Upgrade",
+    `Sec-WebSocket-Accept: ${hash}`,
+  ];
+  // 返回握手请求的响应信息
+  socket.write(responseHeaders.join("\r\n") + "\r\n\r\n");
+});
+
+server.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
---
+++ src/util.js
@@ -0,0 +1,205 @@
+const crypto = require("crypto");
+const MAGIC_KEY = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+const mask_length = 4;
+const seg_length = 270;
+
+function generateAcceptValue(secWsKey) {
+  return crypto
+    .createHash("sha1")
+    .update(secWsKey + MAGIC_KEY, "utf8")
+    .digest("base64");
+}
+
+class Msg {
+  constructor(buffer) {
+    this.msg = "";
+    this.loadData = 0;
+    this.buffers = [];
+    this.parse(buffer);
+  }
+
+  parse(buffer) {
+    const buffers = this.buffers;
+    if (buffers.length === 0) {
+      buffers.push(buffer);
+      const offset = this.parseLengthAndMask(buffer);
+      this.origin = new Uint8Array(this.length);
+      const length = Math.min(buffer.length - offset, this.length);
+      this.readMaskData(buffer, offset, length);
+      this.loadData += length;
+    } else {
+      this.readMaskData(buffer, 0, buffer.length);
+      this.loadData += buffer.length;
+    }
+  }
+
+  left() {
+    return this.length - this.loadData;
+  }
+
+  isFinish(buffer) {
+    this.isFinish = +buffer.readUInt8(0).toString(2).slice(1) === 1;
+  }
+
+  readMaskData(buffer, offset, length) {
+    const { origin, loadData } = this;
+    for (let i = 0; i < length; i++) {
+      origin[i + loadData] = toSuffixHexAdecimal(buffer.readUInt8(i + offset));
+    }
+
+    return origin;
+  }
+
+  parseLengthAndMask(buffer) {
+    const masks = buffer.readUInt8(1).toString(2);
+    const isMask = masks.slice(0, 1);
+    let length = parseInt(masks.slice(1), 2);
+    let offset = 2;
+    if (length === 126) {
+      length = parseInt(buffer.readUInt16BE(2).toString(16), 16);
+      offset += 2;
+    } else if (length === 127) {
+      length = parseInt(
+        buffer.readUInt32BE(2).toString(16) +
+          buffer.readUInt32BE(6).toString(16),
+        16
+      );
+      offset += 8;
+    }
+    this.length = length;
+    if (isMask) {
+      const maskKey = new Uint8Array(mask_length); // 初始化长度
+      for (let i = 0; i < mask_length; i++) {
+        maskKey[i] = toSuffixHexAdecimal(buffer.readUInt8(i + offset)); // 标志为16进制
+      }
+      this.maskKey = maskKey;
+      offset += mask_length;
+    }
+
+    return offset;
+  }
+
+  maskOp() {
+    const { maskKey, origin } = this;
+    const result = new Uint8Array(origin.length);
+    for (let i = 0, j = 0; i < origin.length; i++, j = i % 4) {
+      result[i] = origin[i] ^ maskKey[j];
+    }
+    return result;
+  }
+}
+
+function decode(datas) {
+  const total = datas.reduce((total, cur) => total + cur.length, 0);
+  const segs = Math.ceil(total / seg_length);
+  let seg = 0,
+    segDataIdx = 0, // 指向当前data的索引
+    dataIdx = 0,
+    data = datas[seg];
+  const result = [];
+  while (seg < segs) {
+    const length = seg === segs - 1 ? total - seg_length * seg : seg_length;
+    const arr = new Uint8Array(length);
+    result.push(arr);
+    seg++;
+    for (let arrIdx = 0; arrIdx < length; arrIdx++) {
+      if (segDataIdx === data.length) {
+        segDataIdx = 0;
+        data = datas[++dataIdx];
+      }
+      arr[arrIdx] = data[segDataIdx++];
+    }
+  }
+  return result.reduce(
+    (str, data) =>
+      (str += decodeURI(
+        Array.from(data)
+          .map((r) => "%" + toHexAdecimal(r))
+          .join("")
+      )),
+    ""
+  );
+}
+
+function toHexAdecimal(num) {
+  return Number(num).toString(16);
+}
+
+const OPCODES = {
+  CONTINUE: 0,
+  TEXT: 1,
+  BINARY: 2,
+  CLOSE: 8,
+};
+
+let msgs = [];
+function parseMessage(buffer) {
+  if (msgs.length > 0) {
+    const msg = msgs[msgs.length - 1];
+    const left = msg.left();
+    if (left > 0) {
+      msg.parse(buffer.slice(0, left));
+      if (left >= buffer.length) {
+        return;
+      } else {
+        buffer = buffer.slice(left);
+      }
+    }
+  }
+  const op = buffer.readUInt8(0) & 0x0f;
+  console.log(`op: ${op}`);
+  switch (op) {
+    case OPCODES.TEXT:
+      msgs.push(new Msg(buffer));
+      break;
+    case OPCODES.CONTINUE:
+      const msg = new Msg(buffer);
+      msgs.push(msg);
+      if (msg.isFinish) {
+        const datas = msgs.map((m) => m.maskOp());
+        console.log(`收到信息：${decode(datas)}`);
+        msgs = [];
+      }
+      break;
+    case OPCODES.BINARY:
+      // 暂不处理二进制流
+      break;
+    case OPCODES.CLOSE:
+      console.log("websocket关闭连接");
+      break;
+    default:
+      throw new Error("未知的操作码");
+  }
+}
+
+function log(data) {
+  return Array.from(data)
+    .map((code) => toHexAdecimal(code))
+    .join(",");
+}
+
+// 只处理长度小于125的字符串
+function send(data) {
+  const b1 = toSuffixHexAdecimal(0x80 | OPCODES.TEXT);
+  const payLoad = Buffer.from(data, "utf8");
+  const length = payLoad.length;
+  const b2 = toSuffixHexAdecimal(length);
+  const buffer = Buffer.alloc(length + 2);
+  buffer.writeUInt8(b1, 0);
+  buffer.writeUInt8(b2, 1);
+  payLoad.copy(buffer, 2);
+  return buffer;
+}
+
+function toSuffixHexAdecimal(num) {
+  return "0x" + toHexAdecimal(num);
+}
+
+function close() {
+  const buffer = Buffer.alloc(2);
+  buffer.writeUInt8(toSuffixHexAdecimal(0x80 | OPCODES.CLOSE));
+  buffer.writeUInt8(toSuffixHexAdecimal(0), 1);
+  return buffer;
+}
+
+module.exports = { generateAcceptValue, parseMessage, log, send, close };
